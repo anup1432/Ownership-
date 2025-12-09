b5b5b6866cc676925dddc63ee6627b24bb6303c8
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-12-09 08:28:59 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 client = MongoClient(os.getenv('MONGO_URI'))
-db = client.get_default_database() if client else client['tgownership']
+
+# FIX: never use get_default_database()
+db = client["tgownership"]
+
 users_col = db['users']
 groups_col = db['groups']
 withdrawals_col = db['withdrawals']
